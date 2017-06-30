f8f82f0b9174b94cf84519545111c636e7edefc3
Aleksandr Sokolovskii <amsokol@gmail.com>
2017-06-30 18:25:10 +0300

    cacheName is optional

--- http/v1/increment.go
+++ http/v1/increment.go
@@ -13,7 +13,9 @@ import (
 func (c *client) Increment(cache string, key string, init *int64, delta int64) (value int64, nodeID string, token string, err error) {
 	v := url.Values{}
 	v.Add("cmd", "incr")
-	v.Add("cacheName", cache)
+	if len(cache) > 0 {
+		v.Add("cacheName", cache)
+	}
 	v.Add("key", key)
 	if init != nil {
 		v.Add("init", strconv.FormatInt(int64(*init), 10))
